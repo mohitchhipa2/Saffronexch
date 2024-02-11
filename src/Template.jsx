f2465391c052212107df7fdc6d47c4c72d56bba4
Mohit Chhipa <mohitchhipa8107@gmail.com>
2024-02-11 15:38:02 +0530

    final upload

---
+++ src/Template.jsx
@@ -0,0 +1,156 @@
+import React from 'react'
+import "./Template.css"
+import '../src/css/bootstrap.min.css';
+import '../src/css/animate.min.css';
+// import '../src/css/magnific-popup.css';
+// import '../src/css/all.min.css';
+// import 'odometer/themes/odometer-theme-default.css';
+import '../src/css/aos.css';
+// import '../src/css/owl.carousel.css';
+import '../src/css/meanmenu.css';
+import '../src/css/slick.css';
+import '../src/css/default.css';
+import '../src/css/style.css';
+import '../src/css/responsive.css';
+import { FaInstagram } from "react-icons/fa";
+import { FaTelegram } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+
+
+const Template = () => {
+    return (
+        <div>
+            <header>
+                <div className="header-top-area s-header-top-area d-none d-lg-block">
+                    <div className="container-fluid s-container-full-padding">
+                        <div className="row align-items-center">
+                            <div className="col-lg-6 d-none d-lg-block">
+                                <div className="header-top-offer">
+                                    <p>Get Trusted Online ID Here</p>
+                                </div>
+                            </div>
+                            <div className="col-lg-6">
+                                <div className="header-top-right">
+                                    <div className="header-social">
+                                        <ul>
+                                            <ul className="sm-megamenu-hover sm_megamenu_menu sm_megamenu_menu_black">
+                                                {/*                        <li><a href="index.php">Home</a></li>*/}
+                                                <li><a href="./">Home</a>
+                                                    {/*<ul>
+                     <li><a href="#">Food & Experience</a></li>
+                     <li><a href="#">Brand Legacy</a></li>
+                     <li><a href="#">Our Team</a></li>
+                 </ul>*/}
+                                                </li>
+                                                <li><a href="contact.html">Contact Us</a></li>
+                                                <li><a href="privacy-policy.html">Privacy Policy</a></li>
+                                            </ul>
+                                        </ul>
+                                    </div>
+                                    <div className="header-top-action">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+
+
+
+            <main>
+                <a target="_blank" href="https://api.whatsapp.com/send?phone=917357882855&text=Hello%20Maharaja%2C%20I%20want%20Online%20Cricket%20ID%20with%2010%25%20Extra%20bonus%20Source%3A-%20MMC%2C%20AD" className="enq-side-btn-icone"><i className="fab fa-whatsapp" /> Click Here to Get ID</a>
+                <section className="slider-area slider-bg">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-xl-9">
+                                <div className="slider-content text-center">
+                                    <div className="logo">
+                                        <a target="_blank" href="https://api.whatsapp.com/send?phone=917357882855&text=Hello%20Maharaja%2C%20I%20want%20Online%20Cricket%20ID%20with%2010%25%20Extra%20bonus%20Source%3A-%20MMC%2C%20AD"><img src="https://sportsidprovider.online/img/lightLogo.png" alt="Logo" width="200px" /></a>
+                                    </div>
+                                    <h6 className="wow fadeInDown" data-wow-delay=".2s">Maharaja [10 साल का भरोसा] - Get your Trusted Fantasy Sport</h6>
+                                    <h2 className="tlt fix" data-in-effect="fadeInLeft">Trusted daily fantasy sport <span>10% Welcome Bonus</span></h2>
+                                    <p className="wow fadeInUp" data-wow-delay="2s">India's no. 1 Daily online Fantasy Sport games promoter. Create your Account within 2 minutes! Join Most Trusted, Secure &amp; Fastest Fantasy Sport of India.</p>
+                                    <a target="_blank" href="https://api.whatsapp.com/send?phone=917357882855&text=Hello%20Maharaja%2C%20I%20want%20Online%20Cricket%20ID%20with%2010%25%20Extra%20bonus%20Source%3A-%20MMC%2C%20AD" className="btn wow fadeInUp" data-wow-delay="2.2s">Whatsapp Now For Create Id</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className="game-single-area pt-120 pb-120">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12">
+                                <div className="game-single-content">
+                                    <div className="upcoming-game-head">
+                                        <div className="uc-game-head-title">
+                                            <h4>welcome in The Family of <span>Maharaja "Khelo Dil and Dimag Se" - Oldest Fantasy Sports provider</span></h4>
+                                        </div>
+                                        <a target="_blank" href="https://api.whatsapp.com/send?phone=917357882855&text=Hello%20Maharaja%2C%20I%20want%20Online%20Cricket%20ID%20with%2010%25%20Extra%20bonus%20Source%3A-%20MMC%2C%20AD">
+                                            <div className="uc-game-price">
+                                                <h5>Get Your ID NOw</h5>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <p>Get in touch with Maharaja for any Queries, Emergencies, Feedback, or Complaints. We are here to help you 24*7 with our services on WhatsApp.</p>
+                                    <div className="game-single-img">
+                                        <div className="row">
+                                            <div className="col-sm-12">
+                                                <a href="https://wa.me/919999999999?text=Helloooo%20I%20need%20ID"> </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <p>Welcome to Maharaja Book, we offer you a genuinely unique fantasy sports experience. Besides, We have a global network and provide the best industry experience with a single click.</p>
+                                    <p>Maharaja Book is a responsible fantasy sports promoter. We deal in all kinds of fantasy sports games. We have helped 50000+ players to find the best fantasy sports online.</p>
+                                    <p>Our Company Works In A Very Transparent Way. Get Your Demo ID Now</p>
+                                    <div className="game-single-title mt-60 mb-30">
+                                        <h4>Why Choose Us</h4>
+                                    </div>
+                                    <div className="game-single-info mb-65">
+                                        <ul>
+                                            <li>24*7 Support</li>
+                                            <li>Instant Reply</li>
+                                            <li>Most Trusted &amp; Secure Platform</li>
+                                            <li>10 Lac+ Active Users</li>
+                                            <li>250+ Branches</li>
+                                            <li>Working Since 2010</li>
+                                        </ul>
+                                    </div>
+                                    <div className="footer-widget mb-50">
+                                        <div className="fw-title mb-35">
+                                            <h5>Follow us</h5>
+                                        </div>
+                                        <div className="footer-social">
+                                            <ul>
+                                                <li><a href="#"><FaInstagram /></a></li>
+                                                <li><a href="#"><FaTelegram /></a></li>
+                                                <li><a href="#"><FaWhatsapp /></a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+
+
+            <footer>
+                <div className="footer-top footer-bg s-footer-bg">
+                    <div className="container">
+                        <ul className="sm-megamenu-hover sm_megamenu_menu sm_megamenu_menu_black sdfgfd">
+                            <li><a href="terms-and-conditions.html">Terms and conditions</a></li>
+                            <li><a href="game-addiction.html">Game Addiction</a></li>
+                            <li><a href="privacy-policy.html">Privacy Policy</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </footer>
+
+
+        </div>
+    )
+}
+
+export default Template
